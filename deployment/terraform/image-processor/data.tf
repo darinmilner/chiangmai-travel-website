39f1e52f4107f84d8c8a3d472df640b8988ff41a
Darin Milner <darinmcodingprojects@gmail.com>
2026-09-11 22:48:35 +0700

    Lambda Layer variable name changed.

--- deployment/terraform/image-processor/data.tf
+++ deployment/terraform/image-processor/data.tf
@@ -14,5 +14,5 @@ data "aws_s3_bucket" "image_bucket" {
 
 data "aws_lambda_layer_version" "shared_layer" {
   layer_name = "${var.layer_name}-${var.environment}-${local.short_region}"
-  version = 1
+  version    = 1
 }
