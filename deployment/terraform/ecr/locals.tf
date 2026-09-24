a94f1b1d0c81d33bad17b38c0f85d985ca48a890
Darin Milner <darinmcodingprojects@gmail.com>
2026-09-24 18:43:08 +0700

    ECR and Route53 an ACM Terraform modules added.

--- deployment/terraform/ecr/locals.tf
+++ deployment/terraform/ecr/locals.tf
@@ -2,6 +2,6 @@ locals {
   region         = "ap-southeast-7"
   app_name       = "ChiangMaiHalal"
   app_name_lower = lower(local.app_name)
-  environment    = beta
+  environment    = "beta"
   short_region   = replace(local.region, "-", "")
 }
